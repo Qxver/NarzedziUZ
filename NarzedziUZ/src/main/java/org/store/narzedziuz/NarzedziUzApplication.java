a68ffd3e707cae7dbf461a0e1c1953ab5677ecbb
Wojciech Paradziński <paradzinskiwojciech@gmail.com>
2025-10-23 20:24:50 +0200

    Spring boot template, index.html test

---
+++ NarzedziUZ/src/main/java/org/store/narzedziuz/NarzedziUzApplication.java
@@ -0,0 +1,13 @@
+package org.store.narzedziuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarzedziUzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarzedziUzApplication.class, args);
+	}
+
+}
