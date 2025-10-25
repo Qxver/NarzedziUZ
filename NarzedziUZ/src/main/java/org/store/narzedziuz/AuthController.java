c74bf63112327dd99f655f47509acef90a684261
Wojciech Paradziński <paradzinskiwojciech@gmail.com>
2025-10-25 19:12:37 +0200

    Login/register plain page

---
+++ NarzedziUZ/src/main/java/org/store/narzedziuz/AuthController.java
@@ -0,0 +1,16 @@
+package org.store.narzedziuz;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
+    }
+}
