babc6e94d9b1001e9d553060a256aa4b7440e4d2
Wojciech Paradziński <paradzinskiwojciech@gmail.com>
2025-11-22 12:16:21 +0100

    Login

--- NarzedziUZ/src/main/java/org/store/narzedziuz/HomeController.java
+++ NarzedziUZ/src/main/java/org/store/narzedziuz/HomeController.java
@@ -1,12 +1,21 @@
 package org.store.narzedziuz;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String index() {
+    public String index(HttpSession session, Model model) {
+        Object user = session.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", session.getAttribute("userName"));
+            model.addAttribute("isLoggedIn", true);
+        } else {
+            model.addAttribute("isLoggedIn", false);
+        }
         return "home";
     }
-
 }
